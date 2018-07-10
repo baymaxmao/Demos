b89454ce6135a9ce5e28cfab5b01ec477c2a408a
baymaxmao <msl1754608859@163.com>
2018-07-10 14:34:44 +0800

    fisrt demo to use .net mvc

---
+++ TestMVC/TestMVC/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class Employee
+    {
+        public string FirstName
+        {
+            get;set;
+        }
+        public string LastName
+        {
+            get;set;
+        }
+        public int Salary
+        {
+            get;set;
+        }
+    }
+}
---
+++ TestMVC/TestMVC/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee emp = new Employee();
+            emp.FirstName = "johnson";
+            emp.LastName = "fernandes";
+            emp.Salary = 140000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "michael";
+            emp.LastName = "jackson";
+            emp.Salary = 160000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "robert";
+            emp.LastName = "pattinson";
+            emp.Salary = 200000;
+            employees.Add(emp);
+
+            return employees;
+        }
+    }
+}
